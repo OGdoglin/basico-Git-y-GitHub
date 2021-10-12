22b3b94785bb632df343c6f2371b78f50c289d26
Dacota <logy.thunder1028@gmail.com>
2021-10-12 05:08:42 -0500

    buble y bucle

--- despedida/despedida.cpp
+++ despedida/despedida.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+/*
+* Funcion Sumar
+*   párametros x e y de tipo double
+*   devuleve x + y
+*/
+
+double sumar(double x, double y)
+{
+    double resultado = 0;
+    resultado = x + y;
+    return resultado;
+}
+
 int main()
 {
-    cout << "Despedida";
+    double a = 10, b = 20, r = 0;
+    r = sumar(a, b);
+    cout << "Suma = " <<  r << '\n';
 }
